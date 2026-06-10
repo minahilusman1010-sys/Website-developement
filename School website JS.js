faa1df92bad0dc5706cd26faef19d9b6398679cb
minahilusman1010 <minahilusman1010@gmail.com>
2026-06-11 02:00:28 +0500

    hello

--- School website JS.js
+++ School website JS.js
@@ -1,1 +1,27 @@
-var my Index
+
+    var myIndex = 0;
+    carousel();
+
+    function carousel() {
+        var a;
+        var b = document.getElementsByClassName("image");
+
+        // Hide all images
+        for (a = 0; a < b.length; a++) {
+            b[a].style.display = "none";
+        }
+
+        // Move to next image
+        myIndex++;
+
+        if (myIndex > b.length) {
+            myIndex = 1;
+        }
+
+        // Show current image
+        b[myIndex - 1].style.display = "block";
+
+        // Change image every 2 seconds
+        setTimeout(carousel, 2000);
+    }
+
